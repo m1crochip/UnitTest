8af65d90f383962dfa87efc3a55f28630fda199b
Thanakit Charoenboonvivat <54524621+m1crochip@users.noreply.github.com>
2022-06-15 03:03:26 +0700

    Final Update merge Unittest

--- merge.test.ts
+++ merge.test.ts
@@ -1,8 +1,61 @@
-const mergeTest = require('./merge');
-test('Test merge sort function [-1] ,[-3] = [-1,-3]', () => {
- expect(mergeTest([-1], [-3])).toEqual([-3, -1])
-})
+const mergeTest = require("./merge");
 
-test('Test merge sort function [-1,2,0,3,-8] ,[-3,-4,1,2] = [-8,-4,-3,-1,0,1,2,2,3]', () => {
- expect(mergeTest([-1,2,0,3,-8],[-3,-4,1,2])).toEqual([-8,-4,-3,-1,0,1,2,2,3])
-})
+/////The Possitive Number
+test("Test merge sort function [1,2,3], [4,5,6] = [1,2,3,4,5,6]", () => {
+  expect(mergeTest([1, 2, 3], [4, 5, 6])).toEqual([1, 2, 3, 4, 5, 6]);
+});
+
+test("Test merge sort function [3,2,1] ,[6,5,4] = [1,2,3,4,5,6]", () => {
+  expect(mergeTest([3, 2, 1], [6, 5, 4])).toEqual([1, 2, 3, 4, 5, 6]);
+});
+
+test("Test merge sort function [1,1,1], [1,1,1] = [1,1,1,1,1,1]", () => {
+  expect(mergeTest([1, 1, 1], [1, 1, 1])).toEqual([1, 1, 1, 1, 1, 1]);
+});
+
+test("Test merge sort function [1,2,3] ,[1,2,3] = [1,1,2,2,3,3]", () => {
+  expect(mergeTest([1, 2, 3], [1, 2, 3])).toEqual([1, 1, 2, 2, 3, 3]);
+});
+
+/////The Negative numbers
+
+test("Test merge sort function [-1,-2,-3], [-4,-5,-6] = [-6,-5,-4,-3,-2,-1]", () => {
+  expect(mergeTest([-1, -2, -3], [-4, -5, -6])).toEqual([
+    -6, -5, -4, -3, -2, -1,
+  ]);
+});
+
+test("Test merge sort function [-3,-2,-1] ,[-6,-5,-4] = [-6,-5,-4,-3,-2,-1]", () => {
+  expect(mergeTest([-3, -2, -1], [-6, -5, -4])).toEqual([
+    -6, -5, -4, -3, -2, -1,
+  ]);
+});
+
+test("Test merge sort function [-1,-1,-1], [-1,-1,-1] = [-1,-1,-1,-1,-1,-1]", () => {
+  expect(mergeTest([-1, -1, -1], [-1, -1, -1])).toEqual([
+    -1, -1, -1, -1, -1, -1,
+  ]);
+});
+
+test("Test merge sort function [-1,-2,-3] ,[-1,-2,-3] = [-3,-3,-2,-2,-1,-1]", () => {
+  expect(mergeTest([-1, -2, -3], [-1, -2, -3])).toEqual([
+    -3, -3, -2, -2, -1, -1,
+  ]);
+});
+
+//// Mix Number
+test("Test merge sort function [-1,2,0,3,-8] ,[-3,-4,1,2] = [-8,-4,-3,-1,0,1,2,2,3]", () => {
+  expect(mergeTest([-1, 2, 0, 3, -8], [-3, -4, 1, 2])).toEqual([
+    -8, -4, -3, -1, 0, 1, 2, 2, 3,
+  ]);
+});
+
+//////The empty list
+test("Test merge sort function [] ,[] = []", () => {
+  expect(mergeTest([], [])).toEqual([-1]);
+});
+
+////The Character
+test("Test merge sort function [a,b,c] ,[d,e,f] = [a,b,c,d,e,f]", () => {
+  expect(mergeTest(["a", "b", "c"], ["d", "e", "f"])).toEqual([-1]);
+});
--- merge.ts
+++ merge.ts
@@ -1,8 +1,19 @@
-function merge(collection_1: number[], collection_2: number[]): number[]{
- let result: number[] = [];
- result = collection_1.concat(collection_2).sort((a,b) => a-b)
- // console.log('merge')
- return result
+function merge(collection_1: number[], collection_2: number[]): number[] {
+  let isNumberArray1: boolean =
+    collection_1.length > 0 &&
+    collection_1.every((value) => {
+      return typeof value === "number";
+    });
+  let isNumberArray2: boolean =
+    collection_2.length > 0 &&
+    collection_2.every((value) => {
+      return typeof value === "number";
+    });
+  if (isNumberArray1 && isNumberArray2) {
+    let result: number[] = [];
+    result = collection_1.concat(collection_2).sort((a, b) => a - b);
+    console.log(typeof collection_1);
+    return result;
+  } else return [-1];
 }
-
 module.exports = merge;
